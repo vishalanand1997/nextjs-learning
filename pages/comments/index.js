519d697266be2e22f567799a3f6cf20d494a972e
Vishal anand <vishal@signitysolutions.com>
2022-07-11 15:48:15 +0530

    API POST request

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -2,14 +2,29 @@ import React, { useState } from 'react'
 
 function Comments() {
     const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState('');
 
     const fetchComments = async () => {
         const response = await fetch('/api/comments');
         const data = await response.json();
         setComments(data);
     }
+
+    const submitComment = async () => {
+        const response = await fetch('/api/comments', {
+            method: "POST",
+            body: JSON.stringify({ id: Date.now(), comment }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        console.log("Data", data)
+    }
     return (
         <div>
+            <input type='text' value={comment} onChange={(e) => setComment(e.target.value)} />
+            <button onClick={submitComment}>Submit Comment</button>
             <button onClick={fetchComments}>Load comments</button>
             {comments.map((comment) => (
                 <div key={comment.id}>
